6d3e44180d4721c7d3cf9204fca6b8bb911a0994
Nana <160034837+a-aaangeeel@users.noreply.github.com>
2024-03-09 23:19:58 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,41 +2,41 @@
 #include <cstdint>
 #include "alg.h"
 
-
-double pown(double value, uint16_t n) 
+double pown(double value, uint16_t n)
 {
 	double Result = 1.0;
 	uint16_t i = 0;
-	for ( i = 0; i < n; i++) {
-		Result = value* Result;
+	for (i = 0; i < n; ++i) {
+		Result = value * Result;
 	}
 	return Result;
 }
 
-uint64_t fact(uint16_t n) 
+uint64_t fact(uint16_t n)
 {
 	uint64_t Result = 1;
 	uint16_t i = 0;
-	for ( i = 1; i <= n; i++) {
-		Result = i* Result;
+	for (i = 1; i <= n; ++i) {
+		Result = i * Result;
 	}
 	return Result;
 }
 
-double calcItem(double x, uint16_t n) 
+double calcItem(double x, uint16_t n)
 {
 	double Result = 0;
-	Result = pown(x, n) / fact(n);
+	Result = (double)pown(x, n) / (double)fact(n);
 	return Result;
 }
 
-double expn(double x, uint16_t count) 
+
+double expn(double x, uint16_t count)
 {
 	double Result = 0.0;
 	uint16_t i = 0;
 
-	for ( i = 0; i < count; i++) {
-		Result = Result + calcItem(x, i);
+	for (i = 0; i < count; ++i) {
+		Result = Result + (double)calcItem(x, i);
 	}
 	return Result;
 }
@@ -44,15 +44,15 @@ double expn(double x, uint16_t count)
 double sinn(double x, uint16_t count) {
 	double Result = 0.0;
 	uint16_t i = 0;
-	for ( i = 0; i < count; i++) 
+	for (i = 0; i < count; ++i)
 	{
-		if (i % 2 == 0) 
+		if (i % 2 == 0)
 		{
-			Result = Result +(1 * calcItem(x, 2 * i + 1));
+			Result = Result + ((double)1.0 * (double)calcItem(x, 2 * i + 1));
 		}
 		else
 		{
-			Result = Result + (-1 * calcItem(x, 2 * i + 1));
+			Result = Result + ((double)-1.0 * (double)calcItem(x, 2 * i + 1));
 		}
 	}
 	return Result;
@@ -64,17 +64,21 @@ double cosn(double x, uint16_t count) {
 	double Result = 0.0;
 	uint16_t i = 0;
 
-	for ( i = 0; i < count; i++) {
+	for (i = 0; i < count; i++) {
 
 		if (i % 2 == 0)
 		{
-			Result = Result + (1 * calcItem(x, 2 * i));
+			Result = Result + ((double)1.0 * (double)calcItem(x, 2 * i));
 		}
 		else
 		{
-			Result = Result + (-1 * calcItem(x, 2 * i));
-		}	
+			Result = Result + ((double)-1.0 * (double)calcItem(x, 2 * i));
+		}
 	}
 	return Result;
 }
+
+
+
+
 
