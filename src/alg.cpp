44965646aa6ef15bb98e84e8e14c66cbfe27dc7d
Nana <160034837+a-aaangeeel@users.noreply.github.com>
2024-03-09 22:17:06 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,26 +3,78 @@
 #include "alg.h"
 
 
-double pown(double value, uint16_t n) {
-
+double pown(double value, uint16_t n) 
+{
+	double Result = 1.0;
+	uint16_t i = 0;
+	for ( i = 0; i < n; i++) {
+		Result = value* Result;
+	}
+	return Result;
 }
 
-uint64_t fact(uint16_t n) {
-
+uint64_t fact(uint16_t n) 
+{
+	uint64_t Result = 1;
+	uint16_t i = 0;
+	for ( i = 1; i <= n; i++) {
+		Result = i* Result;
+	}
+	return Result;
 }
 
-double calcItem(double x, uint16_t n) {
-
+double calcItem(double x, uint16_t n) 
+{
+	double Result = 0;
+	Result = pown(x, n) / fact(n);
+	return Result;
 }
 
-double expn(double x, uint16_t count) {
+double expn(double x, uint16_t count) 
+{
+	double Result = 0.0;
+	uint16_t i = 0;
 
+	for ( i = 0; i < count; i++) {
+		Result = Result + calcItem(x, i);
+	}
+	return Result;
 }
 
 double sinn(double x, uint16_t count) {
-
+	double Result = 0.0;
+	uint16_t i = 0;
+	for ( i = 0; i < count; i++) 
+	{
+		if (i % 2 == 0) 
+		{
+			Result = Result +(1 * calcItem(x, 2 * i + 1));
+		}
+		else
+		{
+			Result = Result + (-1 * calcItem(x, 2 * i + 1));
+		}
+	}
+	return Result;
 }
+
+
 
 double cosn(double x, uint16_t count) {
+	double Result = 0.0;
+	uint16_t i = 0;
 
+	for ( i = 0; i < count; i++) {
+
+		if (i % 2 == 0)
+		{
+			Result = Result + (1 * calcItem(x, 2 * i));
+		}
+		else
+		{
+			Result = Result + (-1 * calcItem(x, 2 * i));
+		}	
+	}
+	return Result;
 }
+
